ad601a877defad6677f323e89e4fa6bb954207c5
kazuki04 <kazukik0402@gmail.com>
2021-12-29 15:50:40 +0900

    Define verify and duplicated methods

--- sudoku.go
+++ sudoku.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 )
 
+// 0: not entered
+// 1-9: entered
 type Board [9][9]int
 
 func prettify(b Board) string {
@@ -27,6 +29,48 @@ func prettify(b Board) string {
 	}
 	buf.WriteString("+---+---+---+\n")
 	return buf.String()
+}
+
+func duplicated(c [10]int) bool {
+	// k is number value
+	for k, v := range c {
+		// no check
+		if k == 0 {
+			continue
+		}
+		// v is number of appearances
+		if v >= 2 {
+			return true
+		}
+	}
+	return false
+}
+
+func verify(b Board) bool {
+	// check rows
+	for i := 0; i < 9; i++ {
+		//number of appearances
+		var c [10]int
+		for j := 0; j < 9; j++ {
+			c[b[i][j]]++
+		}
+		if duplicated(c) {
+			return false
+		}
+	}
+
+	// check lines
+	for i := 0; i < 9; i++ {
+		// number of appearances
+		var c [10]int
+		for j := 0; j < 9; j++ {
+			c[b[j][i]]++
+		}
+		if duplicated(c) {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
